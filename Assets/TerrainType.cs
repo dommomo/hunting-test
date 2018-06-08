4b79eb1b7f69ba4801e26e0e4b8c0e9332d09721
Dom Ryan <dom1600@hotmail.com>
2018-06-08 18:01:16 +0930

    added start of terrain manager and some terrain graphics

---
+++ Assets/TerrainType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class TerrainType {
+
+    public string terrainName;
+    public bool walkable;
+    public Sprite[] tiles;
+
+    public Sprite GetTile(Vector2 pos, string seed)
+    {
+        return tiles[RandomHelper.Range(pos, seed.GetHashCode(), tiles.Length)];
+    }
+
+}
